57d231e373135e2e80a15ce447b06f95f33d8b93
Roohan Kebria <rohankebria@gmail.com>
2021-02-23 12:35:40 +0100

    using this in React

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -5,3 +6,25 @@ const element = <h1>Hello World</h1>;
 console.log(element);
 
 ReactDOM.render(element, document.getElementById("root"));
+*/
+class Person {
+  // PascalNotation naming!
+  constructor(name) {
+    this.name = this; // this return reference to the containing object
+  }
+  walk() {
+    console.log("Walk");
+  }
+}
+class Teacher extends Person {
+  constructor(name, subject) {
+    super(name); // as we have added a custom constructor in child class, so have to call parent constructor first
+    this.subject = subject;
+  }
+  teach() {
+    console.log("Teach");
+  }
+}
+
+const teacher = new Teacher("Mosh", "JavaScript");
+console.log(teacher.name);
